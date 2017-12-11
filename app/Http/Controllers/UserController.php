6473e3bcf7e3d0e7210be2cdd7440ffbca7dedb1
Aaron <1399959932@qq.com>
2017-12-11 19:23:25 +0800

    陈,完美状态

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,13 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use DB;
+use Hash;
+use Mail;
+
+//引用对应的命名空间
+use Gregwar\Captcha\CaptchaBuilder;
+use Session;
+use Gregwar\Captcha\PhraseBuilder;
 
 class UserController extends Controller
 {
@@ -159,5 +166,84 @@ class UserController extends Controller
         }else{
             return back()->with('msg','删除失败');
         }
+    }
+
+
+
+    ////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录////////////////////我是注册登录
+     public function zhuce(){
+        return view('zhuce.zhuce');
+    }
+
+
+    public function captcha()
+    {
+        //生成验证码图片的Builder对象，配置相应属性
+        $builder = new CaptchaBuilder;
+        //可以设置图片宽高及字体
+        $builder->build();
+        //获取验证码的内容
+        $phrase = $builder->getPhrase();
+        // var_dump($phrase);
+        
+        //把内容存入session
+        session(['milkcaptcha'=> $phrase]);
+        //生成图片
+        header("Cache-Control: no-cache, must-revalidate");
+        header('Content-Type: image/jpeg');
+        $builder->output();
+    }
+
+    //验证码验证
+    // public function getCode(Request $request)
+    // {
+    //  $userInput = $request->get('captcha');
+    //  if (Session::get('milkcaptcha') == $userInput) {
+    //  //用户输入验证码正确
+    //  return '您输入验证码正确';
+    //  } else {
+    //  //用户输入验证码错误
+    //  return '您输入验证码错误';
+    //  }
+
+    // }
+
+    public function zhucet(Request $request){
+
+        //验证吗
+        $code = $request->code;
+        if(session('milkcaptcha') != $code){
+            return back()->with('msg','验证码错误');
+        }
+
+
+        $data = $request->only(['username','tel','password']);
+        $data['password'] = Hash::make($data['password']);
+
+        //dd($data);
+
+        //插入数据库
+        if (DB::table('users')->insert($data)) {
+
+            Mail::send('email.send', [], function ($message) {
+            //标题
+            $message->subject('ABC');
+            //接收者
+            $message->to('1399959932@qq.com');
+            });
+            return redirect('/')->with('msg','ok');
+        }
+        
+        else{
+            return back()->with('msg','orr');
+        }
+    }
+
+    public function login(){
+        return view('login.login');
+    }
+
+    public function dologin(){
+        $user = DB::table('users')->where('verify',$tel)->first();
     }
 }
