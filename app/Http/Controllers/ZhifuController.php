81d0b03a49d060f17fbfcc6266436c640763ea53
tian <2064219734@qq.com>
2017-12-11 23:21:29 +0800

    rongtan add some feather

---
+++ app/Http/Controllers/ZhifuController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ZhifuController extends Controller
+{
+    public function zfym()
+    {
+    	return view('homes.zfym');
+    }
+}
---
+++ resources/views/homes/zfym.blade.php
@@ -0,0 +1,42 @@
+<link rel="stylesheet" href="./bootstrap/css/bootstrap.min.css">
+<link rel="stylesheet" href="./bootstrap/css/bootstrap-theme.min.css">
+<script type="text/javascript" src="./bootstrap/js/jquery.js"></script>
+<script type="text/javascript" src="./bootstrap/js/bootstrap.min.js"></script>
+<script src="./bootstrap/js/holder.min.js"></script>
+<link rel="stylesheet" type="text/css" href="./css/zfym.css">
+<!-- 支付页面 开始 -->
+	<div class="container">
+		<div class=" zhifu">
+			<div class="zhifu_top">
+			    <img src="holder.js/208x32">
+			    <div class="xian"></div>
+			    <h5>收银台</h5>
+			</div>
+			<div class="zhifu_cen col-md-12">
+				<img src="holder.js/65x65">
+				<div class="cen_middler col-md-5">
+					<p>订单成功只差付款了</p>
+					<p>请您在23小时59分50秒 内完成支付，否则订单将自动取消。</p>
+				</div>
+				<div class="zhifu_right col-md-2 pull-right">
+					<p>订单金额：<span>¥899.00</span></p>
+					<select>
+						<option>订单详情</option>
+					</select>
+				</div>
+			</div>
+			<div class="zhifu_bot col-md-12">
+				<ul class="nav nav-tabs nav-justified">
+				    <li role="presentation" class="active"><a href="#">Home</a></li>
+				    <li role="presentation"><a href="#">Profile</a></li>
+				    <li role="presentation"><a href="#">Messages</a></li>
+				</ul>
+				<ul class="nav nav-pills nav-justified">
+				 <li>s</li>
+				 <li>a</li>
+				 <li>q</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+<!-- 支付页面 结束 -->
