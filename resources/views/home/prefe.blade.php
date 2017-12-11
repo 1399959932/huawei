58c813e248397906a9f51b29362526adcd1162ad
ahao <1159647291@qq.com>
2017-12-11 21:13:51 +0800

    ahao

--- resources/views/home/prefe.blade.php
+++ resources/views/home/prefe.blade.php
@@ -86,21 +86,131 @@
 			<div class="b_b">
 				<div class="col-md-4" style="padding: 0px;">
 					<img src="holder.js/386x190?bg=#aef">
-                    <div class="jie">
+                    <div class="jie shao">
                         <h3><a href="#">华为平板M3&nbsp;青春版8英寸</a></h3>
-                        <p><a href="#">EMUI5.1流畅体验</a></p>
-                        <p><a href="#">&yen;1749</a></p>
+                        <p class="lc"><a href="#">EMUI5.1流畅体验</a></p>
+                        <p class="jia">
+                            <span></span>
+                            <a href="#">&yen;1749</a>
+                        </p>
                     </div>
 				</div>
 				<div class="col-md-4" style="margin-left: -10px;">
 					<img src="holder.js/386x190?bg=#aef">
+                    <div class="jie">
+                        <h3><a href="#">华为平板M3&nbsp;青春版8英寸</a></h3>
+                        <p class="lc"><a href="#">EMUI5.1流畅体验</a></p>
+                        <p class="jia">
+                            <span></span>
+                            <a href="#">&yen;1749</a>
+                        </p>
+                    </div>
 				</div>
-				<div class="col-md-4" style="margin-left: 5px;">
+				<div class="col-md-4" style="margin-left: 9px;">
 					<img src="holder.js/386x190?bg=#aef">
+                    <div class="jie">
+                        <h3><a href="#">华为平板M3&nbsp;青春版8英寸</a></h3>
+                        <p class="lc"><a href="#">EMUI5.1流畅体验</a></p>
+                        <p class="jia">
+                            <span></span>
+                            <a href="#">&yen;1749</a>
+                        </p>
+                    </div>
 				</div>
 			</div>
 		</div>
 	</div>
 </div>
 <!-- 华为精品部分结束 -->
+<!-- 手机部分开始 -->
+<div class="container">
+    <div class="cell">
+        <div class="cell_t">
+            <span></span>
+            <h2><a href="#">手机</a></h2>
+        </div>
+        <div class="cell_c">
+            <ul>
+                <li class="mate"><a href="#">HUAWEI&nbsp;Mate系列</a></li>
+                <li><a href="#">HUAWEI&nbsp;P系列</a></li>
+                <li><a href="#">HUAWEI&nbsp;nova系列</a></li>
+                <li><a href="#">HUAWEI&nbsp;麦芒系列</a></li>
+                <li><a href="#">华为畅享系列</a></li>
+                <li class="gd"><a href="#">更多&nbsp;&nbsp;▶</a></li>
+            </ul>
+        </div>
+        <div class="cell_b">
+            <div class="col-md-3" style="padding: 0px;">
+                <img src="holder.js/291x592?bg=#afe">
+            </div>
+            <div class="col-md-6" style="padding: 0px;">
+                <div class="pp">
+                    <span></span>
+                    <h3><a href="#">HUAWEI&nbsp;nova&nbsp;2&nbsp;Plus</a></h3>
+                    <h5><a href="#">&yen;2399</a></h5>
+                </div>               
+                <img src="holder.js/594x290?bg=#bef">
+                <div class="pin">
+                    <span></span>
+                    <h3><a href="#">HUAWEI&nbsp;nova&nbsp;2&nbsp;Plus</a></h3>
+                    <h5><a href="#">&yen;2399</a></h5>
+                </div>
+                <img src="holder.js/291x290?bg=#bef" style="margin-top: 12px;">
+                <div class="pai">
+                    <span></span>
+                    <h3><a href="#">HUAWEI&nbsp;nova&nbsp;2&nbsp;Plus</a></h3>
+                    <h5><a href="#">&yen;2399</a></h5>
+                </div>
+                <img src="holder.js/291x290?bg=#bef" style="margin-top: 12px; margin-left: 9px;">
+            </div>
+            <div class="col-md-3" style="padding: 9px;margin-top: -9px;">
+                <div class="ppm">
+                    <span></span>
+                    <h3><a href="#">HUAWEI&nbsp;nova&nbsp;2&nbsp;Plus</a></h3>
+                    <h5><a href="#">&yen;2399</a></h5>
+                </div>
+                <img src="holder.js/291x290?bg=#afe">
+                <div class="ming">
+                    <span></span>
+                    <h3><a href="#">HUAWEI&nbsp;nova&nbsp;2&nbsp;Plus</a></h3>
+                    <h5><a href="#">&yen;2399</a></h5>
+                </div>
+                <img src="holder.js/291x290?bg=#afe" style="margin-top: 12px;">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- 手机部分结束 -->
+<!-- 笔记本电脑部分开始 -->
+<div class="container">
+    <div class="note">
+        <div class="note_t">
+            <span></span>
+            <h2><a href="#">笔记本电脑</a></h2>
+        </div>
+        <div class="note_c">
+            <ul>
+                <li class="com"><a href="#">笔记本电脑</a></li>
+                <li><a href="#">笔记本配件</a></li>
+                <li class="gd"><a href="#">更多&nbsp;&nbsp;▶</a></li>
+            </ul>
+        </div>
+        <div class="note_b">
+            <div class="col-md-4" style="padding: 0px;">
+                <img src="holder.js/400x746?bg=#afe">
+            </div>
+            <div class="col-md-4 grid" style="padding: 0px;">
+                <img src="holder.js/400x374?bg=#abc" style="border-top: 1px solid #eaeaea;border-right: 1px solid #eaeaea;">
+                <div class="info"></div>
+            </div>
+            <div class="col-md-4" style="padding: 0px;">
+                <img src="holder.js/400x374?bg=#abc" style="border-top: 1px solid #eaeaea;border-right: 1px solid #eaeaea;">
+            </div>
+            <div class="col-md-8" style="padding: 0px;">
+                <img src="holder.js/800x372?bg=#abc" style="border-bottom: 1px solid #eaeaea; border-top: 1px solid #eaeaea;">
+            </div>
+        </div>
+    </div>
+</div>
+<!-- 笔记本电脑部分结束 -->
 @stop
