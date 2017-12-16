0af1c8bceb6fd3a9b19f949ad076c9ea509fab66
Aaron <1399959932@qq.com>
2017-12-16 19:06:30 +0800

    同12.16

--- app/Http/Middleware/LoginMiddleware.php
+++ app/Http/Middleware/LoginMiddleware.php
@@ -18,11 +18,11 @@ class LoginMiddleware
         $id = session('id');
         //没有登陆
         if(empty($id)) {
-<<<<<<< HEAD
+
             return redirect('/login')->with('msg','没有登陆');
-=======
+
             return redirect('/admin/login')->with('msg','没有登陆');
->>>>>>> 3d5d409c642a8bdc63a60846a47e8a11ff26f503
+
         }
         return $next($request);
     }
